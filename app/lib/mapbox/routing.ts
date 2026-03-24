b04e78390a0100c93f6f9d8cabac52f127bd3abe
nurdjedidi <djedidinur@gmail.com>
2026-03-24 14:16:36 +0100

    'release'

--- app/lib/mapbox/routing.ts
+++ app/lib/mapbox/routing.ts
@@ -1,10 +1,4 @@
-import mapboxgl from "mapbox-gl";
 import { MAPBOX_TOKEN } from "./config";
-console.log("Token chargé :", MAPBOX_TOKEN);
-mapboxgl.accessToken = MAPBOX_TOKEN;
-if (!MAPBOX_TOKEN) {
-  console.error("ATTENTION : Le token Mapbox est manquant dans l'environnement !");
-}
 
 export interface RouteOptions {
   profile?: "driving" | "walking" | "cycling" | "driving-traffic";
@@ -39,7 +33,6 @@ export async function getRoute(
   end: [number, number],
   options: RouteOptions = {}
 ): Promise<RouteData[]> {
-  console.log("Token chargé :", MAPBOX_TOKEN);
   const {
     profile = "driving",
     alternatives = true,
